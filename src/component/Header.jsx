aff56eb2df3515f767e5852c43a88d044ef08ebe
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-01-09 18:06:18 +0530

    updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,11 @@
 import React  from 'react';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import HeaderReUse from '../reusecomponent/HeaderReUse';
 // import getHeaderImg from '../service/HeaderImg';
 import HeaderCover from '../assets/header-background-2.jpg';
+import HeaderCover2 from '../assets/header-background-1.jpg';
+import MySalonLogo from '../assets/beauty-salon_logo_96dp.png';
 import '../style/header.css'
 
 
@@ -33,6 +37,7 @@ function Header(){
     return(
         <>
        <div className='header'>
+        <img src={MySalonLogo} alt='MySalon-logo' className='logo'></img>
          <h2 className='brand-name'>MySolan</h2>
          <nav className='nav-bar'>
            <ul>
@@ -44,9 +49,14 @@ function Header(){
            </ul>
          </nav>
        </div>
+      <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
       <div >
         <img src={HeaderCover} alt='Header-cover' className='header-cover'></img>
       </div>
+      <div>
+        <img src={HeaderCover2} alt='Header-cover' className='header-cover' />
+      </div>
+      </Carousel>
         </>
     )
 }

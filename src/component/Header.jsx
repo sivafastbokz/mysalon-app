725704ffccd5aea717c80307be0b1b20bf19d4fd
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-02-18 15:45:42 +0530

    updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState }  from 'react';
 import { useLocation,useNavigate } from 'react-router-dom';
-import NavMenuReUse from '../reusecomponent/NavMenuReUse';
 import ImageTagReUse from '../reusecomponent/ImageTagReUse';
 import ButtonReUse from '../reusecomponent/ButtonReUse';
 import MySalonLogo from '../assets/beauty-salon_logo_96dp.png';
@@ -9,24 +8,34 @@ import '../style/homepage.css'
 function Header(){
     const [data,setData] = useState('');
     const [showNavBar,setShowNavBar]= useState(false);
-    const preventReload = (event)=>{
-        event.preventDefault();
-    }
     
     const loction = useLocation();
     const navigate = useNavigate();
+
+    const homePage = (event)=>{
+        event.preventDefault();
+        navigate('/mysalon')
+    }
+
+    const servicePage = (event)=>{
+        event.preventDefault();
+        navigate('/mysalon/service')
+    }
+
+    const aboutPage = (event)=>{
+        event.preventDefault();
+        navigate('/mysalon/about')
+    } 
+
+    const contactSection = (event)=>{
+        event.preventDefault();
+        navigate('/mysalon#contactus')
+    }
   
     useEffect(()=>{
         const token = sessionStorage.getItem('token')
         setData(!token)
     },[])
-    
-    const links = [
-        {href:'/mysalon',label:'HOME',nav:{preventReload},className:`header-nav ${loction.pathname === '/mysalon' ? 'active':''}`},
-        {href:'/mysalon/service',label:'SERVICE',nav:{preventReload},className:`header-nav ${loction.pathname === '/mysalon/service' ? 'active':''}`},
-        {href:'/mysalon/about',label:'ABOUT',nav:{preventReload},className:`header-nav ${loction.pathname === '/mysalon/about' ? 'active':''}`},
-        {href:'/mysalon#contactus',label:'CONTACT',nav:{preventReload},className:`header-nav ${loction.pathname === '/mysalon#contactus' ? 'active':''}`},
-    ]
 
     return(
         <>
@@ -35,13 +44,12 @@ function Header(){
          <h2 className='brand-name'>MySalon</h2>
          <nav className={showNavBar ? 'nav-bar expand': 'nav-bar'}>
            <ul>
-            {links.map((link)=>{
-                return(
-                   <NavMenuReUse href={link.href} label={link.label} className={link.className}/>
-                )
-            })}
-            {data ? <ButtonReUse className='header-signup-btn' label='Sign Up' onClick={()=>navigate('/mysalon/signup')}/> 
-            : <ButtonReUse className='myaccount-btn' label='dashboard' onClick={()=>navigate('/mysalon/dashboard')}/> }
+            <li><a href='/mysalon' className={`header-nav ${loction.pathname === '/mysalon' ? 'active':''}`} onClick={homePage}>HOME</a></li>
+            <li><a href='/mysalon/service' className={`header-nav ${loction.pathname === '/mysalon/service' ? 'active':''}`} onClick={servicePage}>SERVICE</a></li>
+            <li><a href='/mysalon/about' className={`header-nav ${loction.pathname === '/mysalon/about' ? 'active':''}`} onClick={aboutPage}>ABOUT</a></li>
+            <li><a href='/mysalon#contactus' className={`header-nav ${loction.pathname === '/mysalon#contactus' ? 'active':''}`} onClick={contactSection}>CONTACT</a></li>
+           <li>{data ? <ButtonReUse className='header-signup-btn' label='Sign Up' onClick={()=>navigate('/mysalon/signup')}/> 
+            : <ButtonReUse className='myaccount-btn' label='dashboard' onClick={()=>navigate('/mysalon/dashboard')}/>}</li>
            </ul>
          </nav>
          <ButtonReUse label={<i class="fa-solid fa-bars fa-2xl"></i>} className='header-menu' onClick={()=>{setShowNavBar(!showNavBar)}}/>

2afbbd35a80d68e55366325888d38fe27546f451
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-03-13 17:07:40 +0530

    updated

--- src/reusecomponent/ButtonReUse.jsx
+++ src/reusecomponent/ButtonReUse.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function ButtonReUse({className,label,onClick,type,value,onSubmit}){
     return <button className={className} onClick={onClick} type={type} value={value} onSubmit={onSubmit}>{label}</button>
-
 }
 
 export default ButtonReUse;

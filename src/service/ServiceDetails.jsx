0986511600134c5bbeb0b48e7c2b6b71d0d6af09
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-03-20 16:09:52 +0530

    updated

--- src/service/ServiceDetails.jsx
+++ src/service/ServiceDetails.jsx
@@ -1,8 +1,8 @@
 import axiosClient from '../helper/AxiosClient';
 
 const getServiceDetails = async()=>{
-  const serviceRes = await axiosClient.get('service/getService').then(res => res.data);
-  return serviceRes
+    const serviceRes = await axiosClient.get('service/getService').then(res => res.data);
+    return serviceRes
 }
 
 export default getServiceDetails;

973148ff96d966c88040bc7c0d0b9dff9ca169c8
sivafastbokz <sivaharshanfastbokz@gmail.com>
2024-05-11 19:19:54 +0530

    updated

--- src/component/DashboardPage.jsx
+++ src/component/DashboardPage.jsx
@@ -9,10 +9,12 @@ import '../style/dashboardpage.css';
 const columns = [
     { field: '_id', headerName: 'ID', width: 130,editable: true },
     { field: 'bookingName', headerName: 'Service Name', width: 130,editable: true },
-    { field: 'bookingDate', headerName: 'Service Date', width: 130,editable: true },
+    { field: 'bookingDate', headerName: 'Service Date', width: 130,editable: true,type: 'date', 
+      valueGetter: (params) => new Date(params.row.bookingDate),
+    },
     { field: 'bookingTime', headerName: 'Service Time', width: 130,editable: true},
     { field: 'bookedDate', headerName: 'Booked Date', width: 130,editable: true},
-    { field: 'Action', headerName: 'Action', width: 130,editable: true},
+    { field: 'Action', headerName: 'Action', type:'action',width: 130,editable: true},
   ];
    
 function DashboardPage(){
